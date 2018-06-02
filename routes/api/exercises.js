41795f40e75a7758c812891e4bc99f390fa5fd7e
Daniel Pimentel <you@example.com>
2018-06-01 21:12:18 -0400

    updates

--- routes/api/exercises.js
+++ routes/api/exercises.js
@@ -3,5 +3,10 @@ const router = express.Router();
 
 router.get('/test', (req,res) => res.json({msg: 'It works'}))
 
+//Get All Exercises
+router.get('/all', (req,res) => res.json({msg: 'All Exercises'}))
+
+//Get Specific Exercise
+router.get('/:exercise'), (req,res) => res.json({msg : '{:exercise}'})
 
 module.exports = router;
